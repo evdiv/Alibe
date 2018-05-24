1d935cb9a3c12aa04505de2cdf45083d843b9fc3
Eugene Dvoir <vakuka@gmail.com>
2018-05-24 15:59:52 -0400

    Update structure

---
+++ src/components/employers-table.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>{{ job.text }}</li>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+        props: ['job', 'id'],
+        methods: mapActions(['updateList']),
+        watch: {
+            'job.checked': function () {
+                this.updateList(this.id)
+            }
+        }
+  }
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+window.axios = require('axios');
+window.Vue = require('vue');
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+import Buefy from 'buefy';
+import VueFormWizard from 'vue-form-wizard';
+import store from './store';
+
+
+import 'vue-form-wizard/dist/vue-form-wizard.min.css';
+
+
+Vue.use(Buefy);
+Vue.use(VueFormWizard); 
+
+Vue.component('employee', require('./components/employee.vue')); 
+Vue.component('employees-table', require('./components/employees-table.vue'));
+Vue.component('employer', require('./components/employer.vue'));
+Vue.component('employers-table', require('./components/employers-table.vue'));
+Vue.component('job', require('./components/job.vue'));
+Vue.component('job-add', require('./components/job-add.vue'));
+Vue.component('jobs-table', require('./components/jobs-table.vue'));
+
+const app = new Vue({
+    el: '#app',
+    store
 });
